a2fa3880b620ffc836ffdeeb07ba2fe52d2fa9f5
Cory Sturgis-Casturgi <casturgi@gmail.com>
2017-02-07 10:17:11 -0500

    added user views and devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, 
+    :password_confirmation, :name, :username, :birthday) }
+
+   devise_parameter_sanitizer.permit(:account_update) { |u| u.permit({ roles: [] }, :email, :password, 
+   :password_confirmation, :current_password, :name, :username, :birthday) }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :comments
+  resources :posts
+
+  get 'your_posts' => 'posts#your_posts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
+
